764ed772749bfda91b58602723533463bdd674bf
ANTH0RNY <nyaganthony@gmail.com>
2022-10-17 09:08:18 +0300

    think im done for now

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -print using weirdcharacters
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+}
