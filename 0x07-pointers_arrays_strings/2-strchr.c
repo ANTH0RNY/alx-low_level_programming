960814a2e2177046369d0c87e3aea68f28471938
ANTH0RNY <nyaganthony@gmail.com>
2022-11-02 22:30:25 +0300

    tried reset again

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,21 +7,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s)
-	{
-		if (*s != c)
-		{
-			s++;
-		}
-		else
-		{
-			return (s);
-		}
-	}
-	if (c == '\0')
-	{
-		return (s);
-	}
+	int i;
 
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
 	return (NULL);
 }
