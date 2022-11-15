7391b2b753fe3c26237f7af8b5ce578e864e8126
ANTH0RNY <nyaganthony@gmail.com>
2022-11-15 14:23:25 +0300

    Last draft

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ *main - print file
  *
  *
- *
- *
+ *Return: void
  */
 
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
