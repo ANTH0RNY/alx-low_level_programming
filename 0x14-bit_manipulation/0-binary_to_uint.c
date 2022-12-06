493c3b0a8d0e9f613690449da286315caaa10f42
ANTH0RNY <nyaganthony@gmail.com>
2022-12-06 15:16:41 +0300

    second draft

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,16 +15,19 @@ unsigned int binary_to_uint(const char *b)
 	i = 0;
 	while (b[i] != '\0')
 	{
-		if (b[i] != '0' && b[1] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		i++;
 	}
+
 	i = 0;
+	j = 0;
 	while (b[i] != '\0')
 	{
-		i <<= 1;
-		if (b[1] == '1')
+		j <<= 1;
+		if (b[i] == '1')
 			j += 1;
+		i++;
 	}
 	return (j);
 }
